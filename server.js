a600e8d42369fb9612fb84cc460da90c0c664610
ryangaraygay <ryan.garaygay@gmail.com>
2014-02-02 01:25:54 -0800

    explicitly set to port 80 for AWSOpsWorks

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.get('/', function(request, response) {
   response.send(fs.readFileSync('index.html').toString());
 });
 
-var port = process.env.PORT || 5000;
+var port = 80;
 app.listen(port, function() {
   console.log("Listening on " + port);
 });
